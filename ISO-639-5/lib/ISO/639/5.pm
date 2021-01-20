74564d84a2c633ad9c40fdf9446fbc6fc119eb4f
Joerg Tiedemann <jorg.tiedemann@helsinki.fi>
2021-01-20 23:35:17 +0200

    changed cai

--- ISO-639-5/lib/ISO/639/5.pm
+++ ISO-639-5/lib/ISO/639/5.pm
@@ -21,11 +21,11 @@ ISO::639::5 - Language groups
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
-our $VERSION      = '0.04';
+our $VERSION      = '0.05';
 
 use Exporter 'import';
 our @EXPORT = qw(
